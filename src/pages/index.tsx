aa96914edfac186b8fdb63acbb8ad63f3a118f2b
Vladislav Guleaev <Vladislav.Guleaev@check24.de>
2023-12-13 22:34:26 +0100

    Add dependency to useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
   useEffect(() => {
     fetchItems();
-  });
+  }, []);
 
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
