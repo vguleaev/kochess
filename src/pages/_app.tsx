8b29da25ef5b616b170ef3f09165290a06b1fc01
Vladislav Guleaev <Vladislav.Guleaev@check24.de>
2023-12-13 21:21:13 +0100

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin', 'cyrillic'], weight: ['400', '700'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Kochess</title>
+        <meta name="description" content="Kochess - Collection of you favorite recipes in your phone" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
