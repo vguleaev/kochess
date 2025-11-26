30def476ca6295b8a1b18f976b5e902a1f08a411
vguleaev <i32vladg@mail.ru>
2025-11-26 21:52:25 +0100

    Add tailwind

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import '../index.css';
 
 const RootLayout = () => (
   <>
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -8,6 +8,8 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
